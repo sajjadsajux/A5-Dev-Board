f9257a192f072ee259d11373756e223daf0e7746
Sajjad Saju <148693286+sajjadsajux@users.noreply.github.com>
2025-03-01 07:02:19 +0600

    plus minus logic added

--- script/index.js
+++ script/index.js
@@ -1,3 +1,53 @@
 document.getElementById("discover-btn").addEventListener("click", function () {
   window.location.href = "../blogs.html";
 });
+
+const taskCount = document.getElementById("taskCount");
+let clickedButtons = 0;
+const taskCountAdd = document.getElementById("taskCountAdd");
+const msgBox = document.getElementById("msgBox");
+
+function handleClick(buttonId) {
+  const btn = document.getElementById(buttonId);
+  let ntaskCount = parseInt(taskCount.innerText);
+  let sum = ntaskCount - 1;
+  taskCount.innerText = sum;
+
+  let ntaskCountAdd = parseInt(taskCountAdd.innerText);
+  let sum2 = ntaskCountAdd + 1;
+  taskCountAdd.innerHTML = sum2;
+
+  const p = document.createElement("p");
+  p.innerText = `
+  You Have Completed the task
+  
+  `;
+  msgBox.appendChild(p);
+
+  btn.disabled = true;
+
+  alert("Board Updated Successfully");
+  clickedButtons++;
+  if (clickedButtons === 6) {
+    alert("Congratss, You've successfully complete all the pending task");
+  }
+}
+
+document.getElementById("btnSix").addEventListener("click", function () {
+  handleClick("btnSix");
+});
+document.getElementById("btnFive").addEventListener("click", function () {
+  handleClick("btnFive");
+});
+document.getElementById("btnFour").addEventListener("click", function () {
+  handleClick("btnFour");
+});
+document.getElementById("btnThree").addEventListener("click", function () {
+  handleClick("btnThree");
+});
+document.getElementById("btnTwo").addEventListener("click", function () {
+  handleClick("btnTwo");
+});
+document.getElementById("btnOne").addEventListener("click", function () {
+  handleClick("btnOne");
+});
